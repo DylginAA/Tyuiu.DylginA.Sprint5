5ce5849e04e2815eae77d8c50e5a53e870b53122
Admin <Admin@HOME-PC>
2024-11-18 19:35:51 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.DylginA.Sprint5.Task0.V25.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.DylginA.Sprint5.Task0.V25.Lib
+{
+    public class DataService : ISprint5Task0V25
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string outputfile = Path.GetTempFileName(); ;
+            double result = Math.Round((3*Math.Pow(x,4)+1)/Math.Pow(x,3),3);
+            File.WriteAllText(outputfile,Convert.ToString(result));
+            return outputfile;
+        }
+    }
+}
---
+++ Tyuyu.DylginA.Sprint5.Task0.V25/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.DylginA.Sprint5.Task0.V25.Lib;
+namespace Tyuiu.DylginA.Sprint5.Task0.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Дылгин А. | ИБКСБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4");
+            Console.WriteLine("* Тема: Одномернные массивы");
+            Console.WriteLine("* Задание #0");
+            Console.WriteLine("* Вариант #25");
+            Console.WriteLine("* Выполнил: Дылгин А. | ИБКСБ-24-1");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:Дано выражение вычислить его значение при x = 3, результат сохранить в текстовый файл OutPutFileTask0.txt и вывести на консоль. " +
+                "Округлить до трёх знаков после запятой.");
+            Console.WriteLine("");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* x = 3");
+            int x = 3;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.SaveToFileTextData(x));
+        }
+    }
+}
