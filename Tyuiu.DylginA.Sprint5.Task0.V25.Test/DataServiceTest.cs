4e0c2077a3fcb25b101186d0c1f89ef64a63832f
Admin <Admin@HOME-PC>
2024-11-18 19:38:16 +0500

    Task0

---
+++ Tyuiu.DylginA.Sprint5.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.DylginA.Sprint5.Task0.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
