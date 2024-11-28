00fa3e0a8f9721cbb868ae20b88da2957d15daf4
Admin <Admin@HOME-PC>
2024-11-28 20:49:40 +0500

    Task7

---
+++ Tyuiu.DylginA.Sprint5.Task7.V30.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.DylginA.Sprint5.Task7.V30.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
